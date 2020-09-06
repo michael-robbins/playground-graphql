0ce8362735e909a8ed22b1c8ec537f20b5c3b0db
Michael Robbins <michael.robbins@servian.com>
2020-09-06 10:03:23 +1000

    Renaming viewer_backend to viewer_service

--- viewer_service/src/viewer_service/asgi.py
+++ viewer_service/src/viewer_service/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for viewer_backend project.
+ASGI config for viewer_service project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'viewer_backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'viewer_service.settings')
 
 application = get_asgi_application()
--- viewer_service/src/viewer_service/wsgi.py
+++ viewer_service/src/viewer_service/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for viewer_backend project.
+WSGI config for viewer_service project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'viewer_backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'viewer_service.settings')
 
 application = get_wsgi_application()
