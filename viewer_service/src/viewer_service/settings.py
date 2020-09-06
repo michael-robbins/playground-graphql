93af240357b07be0629b55693abdfede7a2a3d44
Michael Robbins <michael.robbins@servian.com>
2020-09-06 16:53:52 +1000

    Fixing up django secret variables

--- viewer_service/src/viewer_service/settings.py
+++ viewer_service/src/viewer_service/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'm*!1f64a$xlh0bc29*b&poo=jz1b3b^nuy683o2tgd5_lr1=hg'
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "False").lower() == "True"
 
 ALLOWED_HOSTS = []
 
